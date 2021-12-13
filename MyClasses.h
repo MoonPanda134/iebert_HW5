a27393ea0866e7ef5a004f3a24a436c734a337ed
MoonPanda134 <93020315+MoonPanda134@users.noreply.github.com>
2021-12-12 22:34:45 -0500

    Add files via upload

---
+++ MyClasses.h
@@ -0,0 +1,29 @@
+#ifndef MYCLASSES_H
+#define MYCLASSES_H
+
+#include "listtools.h"
+using namespace std;
+
+template <class T>
+class MyList {
+    public:
+    void insertHead(T theData){
+        
+    }
+    T deleteHead(){
+
+    }
+    bool search(T &target){
+
+    }
+    void display(){
+
+    }
+
+    private:
+    Node<T> *head;
+};
+
+
+
+#endif//MYCLASSES_H
